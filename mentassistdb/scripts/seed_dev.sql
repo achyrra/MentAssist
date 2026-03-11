bda584ec43a9e5a70b9e137b9330daa52b6fd38e
boris-speed <borisspeed@proton.me>
2026-03-10 21:42:47 -0400

    minor seed_dev change

--- mentassistdb/scripts/seed_dev.sql
+++ mentassistdb/scripts/seed_dev.sql
@@ -2,7 +2,7 @@
 INSERT INTO users (id, email, password_hash, role) VALUES
   (1, 'counselor1@mentassist.com', '$2b$12$kXlwBb//4OTjXXKDn6RZxuoEIC0D80/dWYEvOQk.oGref32UKRAhu', 'counselor'), -- replace $realhash with hash generated during setup (see README)
   (2, 'counselor2@mentassist.com', '$2b$12$kXlwBb//4OTjXXKDn6RZxuoEIC0D80/dWYEvOQk.oGref32UKRAhu', 'counselor')
-ON CONFLICT (id) DO UPDATE SET password_hash = EXCLUDED.password_hash;
+ON CONFLICT DO NOTHING;
 
 INSERT INTO clients (id, counselor_id, first_name, last_name, dob) VALUES
   (1, 2, 'Maya',   'Johnson', '1990-03-14'),
