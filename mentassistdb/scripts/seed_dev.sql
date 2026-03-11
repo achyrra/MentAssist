1ae393f97486be43fcac931abd6ce93e1634b9f2
boris-speed <borisspeed@proton.me>
2026-03-10 20:49:52 -0400

    fixed auth, now POST login functions as expected

--- mentassistdb/scripts/seed_dev.sql
+++ mentassistdb/scripts/seed_dev.sql
@@ -1,8 +1,8 @@
 -- DEV ONLY SEED DATA
 INSERT INTO users (id, email, password_hash, role) VALUES
-  (1, 'counselor1@mentassist.com', '7747d47f92cfca63a6e2b50275e23dba8407c30d8ae929a88ddd49a5d3f2d331', 'counselor'), -- replace $realhash with hash generated during setup (see README)
-  (2, 'counselor2@mentassist.com', '7747d47f92cfca63a6e2b50275e23dba8407c30d8ae929a88ddd49a5d3f2d331', 'counselor')
-ON CONFLICT DO NOTHING;
+  (1, 'counselor1@mentassist.com', '$2b$12$kXlwBb//4OTjXXKDn6RZxuoEIC0D80/dWYEvOQk.oGref32UKRAhu', 'counselor'), -- replace $realhash with hash generated during setup (see README)
+  (2, 'counselor2@mentassist.com', '$2b$12$kXlwBb//4OTjXXKDn6RZxuoEIC0D80/dWYEvOQk.oGref32UKRAhu', 'counselor')
+ON CONFLICT (id) DO UPDATE SET password_hash = EXCLUDED.password_hash;
 
 INSERT INTO clients (id, counselor_id, first_name, last_name, dob) VALUES
   (1, 2, 'Maya',   'Johnson', '1990-03-14'),
