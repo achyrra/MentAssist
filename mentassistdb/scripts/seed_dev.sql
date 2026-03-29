f490507d8a512d4639a22c6090948568b416e2a5
anahzik <mgr117@mocs.utc.edu>
2026-03-29 18:47:45 -0400

    Changes to index.html: - session persistence and auth - user auth, current page, current client id, active tab, etc. stored in sessionStorage - info persists on reload - improved navigation; users can return to previous page or home page - resource library persistence via sessionStorage - autofill resource name from filename - auto-select filetype - upload, download, and remove resources - treatment plans can be edited, previewed, and exported to PDF - client info tab updated to meet sponsor preferences
    Changes to seed_dev:
    - replaced hash string with $replaceWithRealHash for better understanding

--- mentassistdb/scripts/seed_dev.sql
+++ mentassistdb/scripts/seed_dev.sql
@@ -1,7 +1,7 @@
 -- DEV ONLY SEED DATA
 INSERT INTO users (id, email, password_hash, role, first_name, last_name) VALUES
-  (1, 'counselor1@mentassist.com', '$2b$12$DsHGz90jge2jmcz/NlK0Qe0o91RZS2LTgHlSgVnxiDbiUoSq6JWD2', 'counselor', 'Counselor', 'One'), 
-  (2, 'counselor2@mentassist.com', '$2b$12$DsHGz90jge2jmcz/NlK0Qe0o91RZS2LTgHlSgVnxiDbiUoSq6JWD2', 'counselor', 'Counselor', 'Two')
+  (1, 'counselor1@mentassist.com', '$replaceWithRealHash', 'counselor', 'Counselor', 'One'), 
+  (2, 'counselor2@mentassist.com', '$replaceWithRealHash', 'counselor', 'Counselor', 'Two')
 ON CONFLICT DO NOTHING;
 
 INSERT INTO clients (id, counselor_id, first_name, last_name, dob) VALUES
