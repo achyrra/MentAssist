85c21a7d1178233c4f944cc400427209d1ec988e
Eli-loft <eli.loft@hotmail.com>
2026-04-04 20:19:35 -0500

    update setup-db.ps1 with correct container name and file paths

--- mentassistdb/scripts/setup-db.ps1
+++ mentassistdb/scripts/setup-db.ps1
@@ -1,15 +1,14 @@
-# Be sure to run from repo root!: .\scripts\setup-db.ps1 
+# Be sure to run from repo root!: .\mentassistdb\scripts\setup-db.ps1
 
 docker compose up -d
 
-Start-Sleep -Seconds 2
+Start-Sleep -Seconds 5
 
-Get-Content .\db\schema.sql | docker exec -i mentassist-db psql -U postgres_admin -d mentassist_app
-Get-Content .\db\roles.sql  | docker exec -i mentassist-db psql -U postgres_admin -d mentassist_app
+Get-Content .\mentassistdb\db\schema.sql | docker exec -i mentassistdb psql -U postgres_admin -d mentassist_app
+Get-Content .\mentassistdb\db\roles.sql  | docker exec -i mentassistdb psql -U postgres_admin -d mentassist_app
 
-# When seed is added in the directory, will run & fill with example treatment plans.
-if (Test-Path .\db\seed.sql) {
-  Get-Content .\db\seed.sql | docker exec -i mentassist-db psql -U postgres_admin -d mentassist_app
+if (Test-Path .\mentassistdb\scripts\seed_dev.sql) {
+  Get-Content .\mentassistdb\scripts\seed_dev.sql | docker exec -i mentassistdb psql -U postgres_admin -d mentassist_app
 }
 
-Write-Host "✅ MentAssist DB ready on localhost:5432 (DB=mentassist_app)"
+Write-Host "MentAssist DB ready on localhost:5432 (DB=mentassist_app)"
